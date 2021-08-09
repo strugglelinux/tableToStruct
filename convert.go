f5aa2550a6938b30bea9461e26c14e3869e59090
李晨曦 <chenxi.li@uhomes.com>
2021-08-09 18:13:08 +0800

    表结构转为结构体

---
+++ convert.go
@@ -0,0 +1,171 @@
+package tableToStruct
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"sync"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const selectSql = `SELECT COLUMN_NAME,DATA_TYPE,TABLE_NAME,COLUMN_COMMENT
+FROM information_schema.COLUMNS 
+WHERE table_schema = DATABASE()`
+
+type Record struct {
+	COLUMN_NAME    string
+	DATA_TYPE      string
+	TABLE_NAME     string
+	COLUMN_COMMENT string
+	IS_NULLABLE    string
+}
+type TableToStruct struct {
+	savePath      string   //生成文件保存路径
+	tables        []string //设置导入哪些表 空时为数据库全部的表
+	db            *sql.DB
+	dsn           string //数据库连接配置
+	selectSql     string
+	structContext chan string
+	mux           *sync.Mutex
+	wg            sync.WaitGroup
+}
+
+func NewTableToStruct() *TableToStruct {
+	t2s := &TableToStruct{}
+	t2s.db = &sql.DB{}
+	t2s.selectSql = selectSql
+	t2s.wg = sync.WaitGroup{}
+	t2s.mux = &sync.Mutex{}
+	t2s.structContext = make(chan string)
+	return t2s
+}
+
+//设置保存路径
+func (t *TableToStruct) SetSavePath(path string) {
+	t.savePath = path
+}
+
+func (t *TableToStruct) SetDbDsn(dsn string) {
+	t.dsn = dsn
+	var err error
+	t.db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalln("数据库连接失败:" + err.Error())
+	}
+	log.Println("数据库连接成功")
+}
+
+//设置要查询哪些表
+func (t *TableToStruct) SetTable(tables []string) {
+	t.tables = tables
+	var whereParams string
+	switch len(tables) {
+	case 0:
+	case 1:
+		whereParams = fmt.Sprintf(" AND TABLE_NAME = '%s' ", tables[0])
+	default:
+		var ts []string
+		for _, t := range tables {
+			ts = append(ts, fmt.Sprintf("'%s'", strings.TrimSpace(t)))
+		}
+		whereParams = fmt.Sprintf(" AND TABLE_NAME IN (%s) ", strings.Join(ts, ","))
+	}
+	t.selectSql += whereParams
+}
+
+//获取表记录信息
+func (t *TableToStruct) getTablesColumns() map[string][]Record {
+	tablesColumns := make(map[string][]Record)
+	rows, err := t.db.Query(t.selectSql)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer rows.Close()
+	for rows.Next() {
+		record := Record{}
+		err = rows.Scan(&record.COLUMN_NAME, &record.DATA_TYPE, &record.TABLE_NAME, &record.COLUMN_COMMENT)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		tablesColumns[record.TABLE_NAME] = append(tablesColumns[record.TABLE_NAME], record)
+	}
+	return tablesColumns
+}
+
+//导出操作
+func (t *TableToStruct) exportStructText(tc chan *Table) {
+	var structContext string
+	var importContext string
+	for {
+		select {
+		case table := <-tc:
+			if table == nil {
+				goto Loop
+			}
+			t.mux.Lock()
+			res := table.handler()
+			if !res {
+				log.Printf("数据表%s处理失败\n", table.Name)
+			}
+			if len(table.Tstruct) > 0 {
+				structContext += "\n" + table.Tstruct
+			}
+			if len(table.ImportTag) > 0 && (strings.Index(importContext, table.ImportTag) == -1) {
+				importContext += table.ImportTag + "\n"
+			}
+			t.mux.Unlock()
+		default:
+		}
+	}
+Loop:
+	t.wg.Done()
+	t.structContext <- fmt.Sprintf("%s \n %s \n\n %s", "package mode", importContext, structContext)
+	close(t.structContext)
+}
+
+//保存内容
+func (t *TableToStruct) saveContext(text string) bool {
+	if len(t.savePath) == 0 {
+		t.savePath = "tableStruct.go"
+	}
+	log.Println("写入文:" + t.savePath)
+	filePath := t.savePath
+	f, err := os.Create(filePath)
+	if err != nil {
+		log.Println("写入文件失败:" + err.Error())
+		return false
+	}
+	defer f.Close()
+	f.WriteString(text)
+	cmd := exec.Command("gofmt", "-w", filePath)
+	cmd.Run()
+	log.Printf("数据保存完成")
+	return true
+}
+
+func (t *TableToStruct) Run() {
+	tablesColumns := t.getTablesColumns()
+	fmt.Println(len(tablesColumns))
+	tableChan := make(chan *Table, len(tablesColumns))
+	for _tablename, _column := range tablesColumns {
+		table := &Table{Name: _tablename, Columns: _column}
+		tableChan <- table
+	}
+	t.wg.Add(1)
+	close(tableChan)
+	go t.exportStructText(tableChan)
+	t.wg.Wait()
+	context := <-t.structContext
+	if len(context) == 0 {
+		log.Println("无表内容需导出")
+		return
+	}
+	s := t.saveContext(context)
+	if s {
+		log.Println("导出完成")
+	}
+}
---
+++ table.go
@@ -0,0 +1,100 @@
+package tableToStruct
+
+import (
+	"fmt"
+	"strings"
+)
+
+var typeForMysqlToGo = map[string]string{
+	"int":                "int",
+	"integer":            "int",
+	"tinyint":            "int",
+	"smallint":           "int",
+	"mediumint":          "int",
+	"bigint":             "int64",
+	"int unsigned":       "int",
+	"integer unsigned":   "int",
+	"tinyint unsigned":   "int",
+	"smallint unsigned":  "int",
+	"mediumint unsigned": "int",
+	"bigint unsigned":    "int64",
+	"bit":                "int",
+	"bool":               "bool",
+	"enum":               "string",
+	"set":                "string",
+	"varchar":            "string",
+	"char":               "string",
+	"tinytext":           "string",
+	"mediumtext":         "string",
+	"text":               "string",
+	"longtext":           "string",
+	"blob":               "string",
+	"tinyblob":           "string",
+	"mediumblob":         "string",
+	"longblob":           "string",
+	"date":               "time.Time", // time.Time or string
+	"datetime":           "time.Time", // time.Time or string
+	"timestamp":          "time.Time", // time.Time or string
+	"time":               "time.Time", // time.Time or string
+	"float":              "float64",
+	"double":             "float64",
+	"decimal":            "float64",
+	"binary":             "string",
+	"varbinary":          "string",
+}
+
+type Table struct {
+	Columns   []Record
+	Name      string
+	Tstruct   string //结构体
+	ImportTag string //导入包
+}
+
+//处理表数据
+func (t *Table) handler() bool {
+	var context string
+	tableName := t.Name
+	l := len(tableName)
+	if l > 1 {
+		tableName = strings.ToUpper(tableName[0:1]) + tableName[1:]
+	} else if l == 1 {
+		tableName = strings.ToUpper(tableName[0:1])
+	}
+	context = fmt.Sprintf(" type %s struct {\n ", tableName)
+	for _, c := range t.Columns {
+		if c.TABLE_NAME != t.Name {
+			continue
+		}
+		field := t.columns(c.COLUMN_NAME)
+		fieldType := typeForMysqlToGo[c.DATA_TYPE]
+		var comment string
+		if len(c.COLUMN_COMMENT) != 0 {
+			comment = strings.ReplaceAll(c.COLUMN_COMMENT, "\n", "")
+			comment = fmt.Sprintf(" // %s", comment)
+		}
+		context += fmt.Sprintf("%s %s  %s \n", field, fieldType, comment)
+	}
+	context += "}\n\n"
+	var importContent string
+	if strings.Contains(context, "time.Time") {
+		importContent = "import \"time\"\n\n"
+	}
+	t.Tstruct = context
+	t.ImportTag = importContent
+	return true
+}
+
+//字段名处理
+func (t *Table) columns(c string) string {
+	var text string
+	for _, f := range strings.Split(c, "_") {
+		switch len(f) {
+		case 0:
+		case 1:
+			text += strings.ToUpper(f[0:1])
+		default:
+			text += strings.ToUpper(f[0:1]) + f[1:]
+		}
+	}
+	return text
+}
